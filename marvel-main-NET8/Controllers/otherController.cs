0804aa87297cf47a5bccbbdb87fe803523bece15
vincent-epro <vincentwschu@eprotel.com.hk>
2025-03-31 14:34:30 +0800

    Add Namespace and rename file

--- marvel-main-NET8/Controllers/otherController.cs
+++ marvel-main-NET8/Controllers/otherController.cs
@@ -8,89 +8,94 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using System.Text.Json.Nodes;
 using System.Reflection;
 
-//[Route("api/[controller]")]
-[Route("api")]
-[ApiController]
-public class OtherController : ControllerBase
+namespace marvel_main_NET8.Controllers
 {
-    private readonly ScrmDbContext _scrme;
 
-    public OtherController(ScrmDbContext context)
+    //[Route("api/[controller]")]
+    [Route("api")]
+    [ApiController]
+    public class OtherController : ControllerBase
     {
-        _scrme = context;
+        private readonly ScrmDbContext _scrme;
+
+        public OtherController(ScrmDbContext context)
+        {
+            _scrme = context;
+        }
+
+        // Check Agent Id
+        [Route("CheckAgentId")]
+        [HttpPost]
+        public IActionResult CheckAgentId([FromBody] JsonObject data)
+        {
+
+            try
+            {
+                bool isExists = userExists(data);
+                return Ok(new { result = isExists });
+            }
+            catch (Exception err)
+            {
+                return Ok(new { result = "fail", details = err.Message });
+            }
+        }
+
+        private bool userExists(JsonObject data)
+        {
+
+            int agentId = Convert.ToInt32((data["AgentID"] ?? "-1").ToString());
+            bool exists = true;
+
+            agentinfo? _agent = (from _a in _scrme.agentinfos
+                                 where _a.AgentID == agentId
+                                 select _a).SingleOrDefault<agentinfo>();
+
+            if (_agent == null)
+            {
+                exists = false;
+            }
+
+            return exists;
+        }
+
+
+        // Check Seller Id
+        [Route("CheckSellerId")]
+        [HttpPost]
+        public IActionResult CheckSellerId([FromBody] JsonObject data)
+        {
+
+            try
+            {
+                bool isExists = sellerIdExists(data);
+                return Ok(new { result = isExists });
+            }
+            catch (Exception err)
+            {
+                return Ok(new { result = "fail", details = err.Message });
+            }
+        }
+
+        private bool sellerIdExists(JsonObject data)
+        {
+
+            string sellerId = (data["SellerID"] ?? "").ToString();
+            bool exists = true;
+
+            agentinfo? _agent = (from _a in _scrme.agentinfos
+                                 where _a.SellerID == sellerId
+                                 select _a).SingleOrDefault<agentinfo>();
+
+            if (_agent == null)
+            {
+                exists = false;
+            }
+
+            return exists;
+        }
+
+
+
     }
-
-    // Check Agent Id
-    [Route("CheckAgentId")]
-    [HttpPost]
-    public IActionResult CheckAgentId([FromBody] JsonObject data)
-    {
-
-        try
-        {
-            bool isExists = userExists(data);
-            return Ok(new { result = isExists });
-        }
-        catch (Exception err)
-        {
-            return Ok(new { result = "fail", details = err.Message });
-        }
-    }
-
-    private bool userExists(JsonObject data)
-    {
-
-        int agentId = Convert.ToInt32((data["AgentID"] ?? "-1").ToString());
-        bool exists = true;
-
-        agentinfo? _agent = (from _a in _scrme.agentinfos
-                             where _a.AgentID == agentId
-                                       select _a).SingleOrDefault<agentinfo>();
-
-        if (_agent == null)
-        {
-            exists = false;
-        }
-
-        return exists;
-    }
-
-
-    // Check Seller Id
-    [Route("CheckSellerId")]
-    [HttpPost]
-    public IActionResult CheckSellerId([FromBody] JsonObject data)
-    {
-
-        try
-        {
-            bool isExists = sellerIdExists(data);
-            return Ok(new { result = isExists });
-        }
-        catch (Exception err)
-        {
-            return Ok(new { result = "fail", details = err.Message });
-        }
-    }
-
-    private bool sellerIdExists(JsonObject data)
-    {
-
-        string sellerId = (data["SellerID"] ?? "").ToString();
-        bool exists = true;
-
-        agentinfo? _agent = (from _a in _scrme.agentinfos
-                             where _a.SellerID == sellerId
-                             select _a).SingleOrDefault<agentinfo>();
-
-        if (_agent == null)
-        {
-            exists = false;
-        }
-
-        return exists;
-    }
-
-
 
 }
